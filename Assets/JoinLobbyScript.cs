0e01de82a377c42cdc0fa60890c17875f00b578f
Ponkapa <djponkapa@gmail.com>
2020-03-14 18:36:35 -0700

    joinLobby

---
+++ Assets/JoinLobbyScript.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class JoinLobbyScript : MonoBehaviour
+{
+    InputField inputField;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        inputField = gameObject.GetComponent<InputField>();
+    }
+
+    public void JoinLobby()
+    {
+        Steamworks.CSteamID cSteamID;
+        cSteamID.m_SteamID = UInt64.Parse(inputField.text);
+        Steamworks.SteamMatchmaking.JoinLobby(cSteamID);
+    }
+}
